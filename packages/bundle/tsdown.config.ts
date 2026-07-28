dd13c5b6ef1e615fcbf473a943bc43c891bc5670
Alex Gorischek <alex.gorischek@gmail.com>
2026-07-28 15:16:35 -0700

    Migrate verify-repo to current tsdown

--- packages/bundle/tsdown.config.ts
+++ packages/bundle/tsdown.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["./src/index.ts", "./src/bin.ts"],
-  noExternal: [/^@verify-repo\//],
-  dts: true,
+  deps: {
+    alwaysBundle: [/^@verify-repo\//],
+  },
+  dts: {
+    eager: true,
+  },
+  fixedExtension: false,
   clean: true,
   outDir: "./dist",
   format: ["esm", "cjs"],
